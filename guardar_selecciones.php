066cc1bcfac76d52e6262281a99318405d69c0e4
aruiz-duxstar <aruiz@duxstar.com>
2023-12-06 22:32:39 -0600

    Automated commit

--- guardar_selecciones.php
+++ guardar_selecciones.php
@@ -1,32 +1,18 @@
 <?php
-// Recibe datos JSON del cuerpo de la solicitud POST
-$data = json_decode(file_get_contents("php://input"), true);
+$votos = json_decode(file_get_contents('php://input'), true);
 
-// Verifica si hay datos recibidos
-if (!empty($data)) {
-    // Genera un nombre de archivo único usando un timestamp
-    $nombreArchivo = 'selecciones_' . date('Ymd_His') . '.csv';
+$csvFileName = 'voto/' . date('Ymd_His') . '_votos.csv';
 
-    // Abre o crea el archivo CSV para escritura
-    $archivoCSV = fopen('voto/' . $nombreArchivo, 'a');
+$csvFile = fopen($csvFileName, 'w');
+fputcsv($csvFile, array('Categoria', 'Nombre'));
 
-    // Encabezados si es un archivo nuevo
-    if (filesize('voto/' . $nombreArchivo) == 0) {
-        fputcsv($archivoCSV, array('Categoria', 'Nombre'));
+foreach ($votos as $categoria => $nombres) {
+    foreach ($nombres as $nombre) {
+        fputcsv($csvFile, array($categoria, $nombre));
     }
-
-    // Escribe las selecciones en el archivo CSV
-    foreach ($data as $seleccion) {
-        fputcsv($archivoCSV, $seleccion);
-    }
-
-    // Cierra el archivo CSV
-    fclose($archivoCSV);
-
-    // Responde con el nombre del archivo guardado
-    echo json_encode(array('success' => true, 'filename' => $nombreArchivo));
-} else {
-    // Responde con error si no hay datos
-    echo json_encode(array('success' => false, 'error' => 'No se recibieron datos.'));
 }
+
+fclose($csvFile);
+
+echo json_encode(['message' => 'Tus votos fueron guardados.']);
 ?>
